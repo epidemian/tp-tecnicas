2f9642ba81c9d373cfee8c0de2546380143c69d5
mleguizamo <mleguizamo@c5b30475-30d6-408d-831a-0400e7537d55>
2010-05-19 04:18:48 +0000

    +Agregado de métodos a dos clases, más una mínima implementación.
    git-svn-id: http://svn2.xp-dev.com/svn/tp-tecnicas/trunk/tp-tecnicas@29 c5b30475-30d6-408d-831a-0400e7537d55

--- src/model/player/Ground.java
+++ src/model/player/Ground.java
@@ -1,5 +1,19 @@
 package model.player;
 
 public class Ground {
-
+	//TODO: How to use tiles?
+	//private tiles;
+	private float price;
+	
+	public void Ground(){
+		//tiles = new ();
+	}
+	
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	public float getPrice() {
+		return price;
+	}
 }
--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -1,5 +1,44 @@
 package model.player;
 
 public class Player {
-
+	private Factory factory;
+	private float initialMoney;
+	private float moneyEarned;
+	private float moneySpent;
+	private float valueToWin;
+	private int dayForPurchaseRent;
+	
+	public void Player(float initialMoney, float valueToWin, int dayForPurchaseRent){
+		this.initialMoney = initialMoney;
+		this.valueToWin = valueToWin;
+		this.dayForPurchaseRent = dayForPurchaseRent;
+		factory = new Factory();
+	}
+	
+	private float GetBalance(){
+		return moneyEarned - moneySpent;	
+	}
+	
+	private boolean LostGame(){
+		//TODO: 
+		float factoryBalanceForDay = 0;
+		return (GetBalance() <= 0) || (factoryBalanceForDay < 0);
+	}
+	
+	private boolean WinGame(){
+		return GetBalance() >= valueToWin;	
+	}
+	
+	private void PayOfRent(){
+		//TODO: Implement Method	
+	}
+	
+	public boolean PurchaseGround(){
+		//TODO: Implement Method
+		return false;	
+	}
+	
+	public void ExecuteDay(){
+		//TODO: Implement Method
+	}
 }
