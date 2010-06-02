7979cbf36438241250eccb42c07e90fd3ef933b0
mleguizamo <mleguizamo@c5b30475-30d6-408d-831a-0400e7537d55>
2010-06-02 03:25:40 +0000

    +Cálculo del Balance diario de la fábrica.
    git-svn-id: http://svn2.xp-dev.com/svn/tp-tecnicas/trunk/tp-tecnicas@129 c5b30475-30d6-408d-831a-0400e7537d55

--- src/model/game/Player.java
+++ src/model/game/Player.java
@@ -1,23 +1,48 @@
 package model.game;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import model.warehouse.Ground;
+import model.warehouse.Warehouse;
 
 import model.game.Budget;
+import model.game.time.TimeManager;
+import model.lab.ResearchLab;
+import model.production.Machine;
+import model.production.ProductionLine;
+import model.production.ProductionLineElement;
 
 public class Player {
 	private Budget budget; 
 	private float valueToWin;
-	
-	public Player(Budget budget, float valueToWin){
+	private TimeManager timeManager;
+		
+	public Player(Budget budget, Warehouse warehouse, ResearchLab researchLab, 
+			float valueToWin, int ticksPerDay, int daysPerMonth)
+	{
 		this.budget = budget;
+		this.timeManager= new TimeManager(ticksPerDay, daysPerMonth); 
 		this.valueToWin = valueToWin;
+		
+		Collection<ProductionLine> productionLines = warehouse.getProductionLines();
+		
+		if(productionLines != null){
+			for (ProductionLine productionLine : productionLines) {
+				timeManager.subscribeTickUpdatable(productionLine); 
+				timeManager.subscribeDailyUpdatable(productionLine);
+			}
+		}
+				
+		timeManager.subscribeDailyUpdatable(researchLab);
+		timeManager.subscribeMonthlyUpdatable(warehouse);
 	}
 	
-	public boolean lostGame(){
-		return budget.getBalance() <= 0;
+	private boolean lostGame(int dayBalance){
+		return (budget.getBalance() <= 0) || (dayBalance < 0);
 	}
 	
-	public boolean winGame(){
+	private boolean winGame(){
 		return budget.getBalance() >= valueToWin;	
 	}
 	
@@ -29,5 +54,21 @@ public class Player {
 		budget.decrement(ground.getPrice());
 		
 		return true;	
+	}
+	
+	public GameState updateTick() {
+		int beforeBalance = budget.getBalance();
+	    timeManager.updateTick();
+	    int affterBalance = budget.getBalance();
+	    
+	    if(winGame()){
+	    	return GameState.WIN;
+	    }else{
+	    	if(lostGame(affterBalance - beforeBalance)){
+	    		return GameState.LOST;
+	    	}
+	    }
+	    
+		return GameState.INPROCESS;
 	}
 }
