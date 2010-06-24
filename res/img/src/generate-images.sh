4cd3f3233d3a8473e005d1a5bb642177c5be62f6
Demian Ferreiro <epidemian@gmail.com>
2010-06-24 19:10:55 +0000

    Imagenes de input/output con sombra
    git-svn-id: http://svn2.xp-dev.com/svn/tp-tecnicas/trunk/tp-tecnicas@348 c5b30475-30d6-408d-831a-0400e7537d55

--- res/img/src/generate-images.sh
+++ res/img/src/generate-images.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 size="-resize 50x50"
 
-convert $size             conveyor_SN.png ../conveyor_SN.png
-convert $size -rotate 90  conveyor_SN.png ../conveyor_WE.png
-convert $size -rotate 180 conveyor_SN.png ../conveyor_NS.png
-convert $size -rotate 270 conveyor_SN.png ../conveyor_EW.png
+convert $size             conveyor_SN.png ../conveyor/conveyor_SN.png
+convert $size -rotate 90  conveyor_SN.png ../conveyor/conveyor_WE.png
+convert $size -rotate 180 conveyor_SN.png ../conveyor/conveyor_NS.png
+convert $size -rotate 270 conveyor_SN.png ../conveyor/conveyor_EW.png
 
-convert $size             conveyor_SE.png ../conveyor_SE.png
-convert $size -rotate 90  conveyor_SE.png ../conveyor_WS.png
-convert $size -rotate 180 conveyor_SE.png ../conveyor_NW.png
-convert $size -rotate 270 conveyor_SE.png ../conveyor_EN.png
+convert $size             conveyor_SE.png ../conveyor/conveyor_SE.png
+convert $size -rotate 90  conveyor_SE.png ../conveyor/conveyor_WS.png
+convert $size -rotate 180 conveyor_SE.png ../conveyor/conveyor_NW.png
+convert $size -rotate 270 conveyor_SE.png ../conveyor/conveyor_EN.png
 
-convert $size -flop             conveyor_SE.png ../conveyor_SW.png
-convert $size -flop -rotate 90  conveyor_SE.png ../conveyor_WN.png
-convert $size -flop -rotate 180 conveyor_SE.png ../conveyor_NE.png
-convert $size -flop -rotate 270 conveyor_SE.png ../conveyor_ES.png
+convert $size -flop             conveyor_SE.png ../conveyor/conveyor_SW.png
+convert $size -flop -rotate 90  conveyor_SE.png ../conveyor/conveyor_WN.png
+convert $size -flop -rotate 180 conveyor_SE.png ../conveyor/conveyor_NE.png
+convert $size -flop -rotate 270 conveyor_SE.png ../conveyor/conveyor_ES.png
 
 
-convert $size             input_N.png ../input_N.png
-convert $size -rotate 90  input_N.png ../input_E.png
-convert $size -rotate 180 input_N.png ../input_S.png
-convert $size -rotate 270 input_N.png ../input_W.png
+convert $size             input_N.png ../input/input_N.png
+convert $size -rotate 90  input_N.png ../input/input_E.png
+convert $size -rotate 180 input_N.png ../input/input_S.png
+convert $size -rotate 270 input_N.png ../input/input_W.png
 
-convert $size             output_N.png ../output_N.png
-convert $size -rotate 90  output_N.png ../output_E.png
-convert $size -rotate 180 output_N.png ../output_S.png
-convert $size -rotate 270 output_N.png ../output_W.png
+convert $size             output_N.png ../output/output_N.png
+convert $size -rotate 90  output_N.png ../output/output_E.png
+convert $size -rotate 180 output_N.png ../output/output_S.png
+convert $size -rotate 270 output_N.png ../output/output_W.png
